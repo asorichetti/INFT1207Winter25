0132fc5476bd833289ffdd938083a2c1e35d29f0
Alex Sorichetti <107128425+asorichetti@users.noreply.github.com>
2025-01-22 15:27:53 -0500

    Feat: Added Comments and main
    adjusted formatting to accomodate a main function
    as well as building in comments to describe
    functionality of certain code elements.

--- Lab1_Group9/password_generator.py
+++ Lab1_Group9/password_generator.py
@@ -22,6 +22,7 @@ def password_inputs(password_length):
                 print("Sorry, the numbers you entered do not add up to your desired password length. \n Please be sure to enter values that will total your desired password length.")
         except ValueError:
             print("Please only input numbers for the prompts, not letters or symbols.")
+    #assigns random letters, numbers, and punctuation to password_holder based on user input
     for i in range (0,a,1):
         password_holder.append(random.choice(string.ascii_letters))
     for i in range (0,b,1):
@@ -33,30 +34,35 @@ def password_inputs(password_length):
 def password_joiner():
     #Shuffles the list into a random order
     random.shuffle(password_holder)
+    #Joins together the elements of password_holder into a string
     password = "".join(password_holder)
     return(password)
 
+#Function to write output into a file
 def write_output_file(file_name, password):
      with open (file_name, "w") as file:
         file.write(f"Password: {password}\n")
 
 
+def main():
+    print("Welcome to the Random Password Generator!")
+    password_checker = 0
+    #Check if password length is in range and make sure user input is valid
+    while password_checker == 0 :
+        try:
+            password_length = int(input("Please input the total length of your password (Minimum of 8 Characters, Maximum of 20): "))
+            if 8 <= password_length <= 20:
+                password_checker = 1
+            else:
+                print("Inputted number outside range, please input a number from 8 to 20.")
+        except ValueError:
+            print("Please input a numeric value and not a string.")
+    password_holder = []
+    password_inputs(password_length)
+    password = password_joiner()
+    print("Strong Password: ", password)
+    write_output_file("Lab1_Group9\Output.txt", password)
+    print("\n Password successfully written to 'Output.txt'")
 
-print("Welcome to the Random Password Generator!")
-password_checker = 0
-#Check if password length is in range and make sure user input is valid
-while password_checker == 0 :
-    try:
-        password_length = int(input("Please input the total length of your password (Minimum of 8 Characters, Maximum of 20): "))
-        if 8 <= password_length <= 20:
-            password_checker = 1
-        else:
-            print("Inputted number outside range, please input a number from 8 to 20.")
-    except ValueError:
-        print("Please input a numeric value and not a string.")
-password_holder = []
-password_inputs(password_length)
-password = password_joiner()
-print("Strong Password: ", password)
-write_output_file("Lab1_Group9\Output.txt", password)
-print("\n Password successfully written to 'Output.txt'")
+if __name__ == "__main__":
+    main()
