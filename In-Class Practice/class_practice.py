98d1b19feea7a44e215016875b05c19495aa7d4b
Alex Sorichetti <107128425+asorichetti@users.noreply.github.com>
2025-01-22 16:00:15 -0500

    Updated for Standards

---
+++ In-Class Practice/class_practice.py
@@ -0,0 +1,13 @@
+class Student:
+    def __init__(self, name, age, student_id):
+        self.name = name
+        self.age = age
+        self.student_id = student_id
+
+    def display_info(self):
+        print(f"Name: {self.name}, Age: {self.age}, Student ID: {self.student_id}")
+
+student1 = Student("Alice", 20, 100998999)
+student2 = Student("Alex", 22, 101000609)
+
+print(f"Name: {student2.name}, Age: {student2.age}, Student ID: {student2.student_id}")
--- Lab1_Group9/README.md
+++ Lab1_Group9/README.md
@@ -1,1 +1,4 @@
-A simple password generator program created by Alex Sorichetti for INFT1207 
+Name: Alex Sorichetti
+Description: A program to randomly generate 8-20 character passwords
+Expected inputs: 4 integer numbers, the first number should be the sum of the following 3 numbers
+Expected Output: A password matching the specifications the user typed in 
